8d78b86e684290ee7296bf7b8d526db3148dbd10
Jakob Odersky <jakob@odersky.com>
2017-12-26 14:07:01 +0100

    Clean up debian configuration files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(nginx-letsencrypt):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="nginx-letsencrypt" command="/usr/bin/nginx-letsencrypt"
--- debian/nginx-letsencrypt.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the nginx-letsencrypt package
-#
-0 4	* * *	root	[ -x /usr/bin/nginx-letsencrypt_maintenance ] && /usr/bin/nginx-letsencrypt_maintenance
